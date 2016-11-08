2229c6a3f4e0ede4940dbeca0dfaf6e906b79728
Marvin Roger <nivramdu94@gmail.com>
2016-11-08 11:23:40 +0100

    :bug: Make dev.bat compatible with default mosquitto install

--- dev.bat
+++ dev.bat
@@ -1,19 +1,18 @@
 @echo off
 
-where npm --version >nul 2>nul
+where npm >nul 2>nul
 if %ERRORLEVEL% neq 0 (
   echo npm not found!
   exit /b 1
 )
 
-where mosquitto -h >nul 2>nul
-if %ERRORLEVEL% neq 0 (
+if not defined MOSQUITTO_DIR (
   echo mosquitto not found!
   exit /b 1
 )
 
 echo Starting MQTT broker (RED)
-start cmd /k "color 4F && mosquitto.exe -v"
+start cmd /k "color 4F && "%MOSQUITTO_DIR%\mosquitto" -v"
 echo Starting development server (BLUE)
 start cmd /k "color 1F && npm run server-dev"
 echo Starting development web app (PURPLE)
