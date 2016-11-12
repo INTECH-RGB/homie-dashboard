acab0d7027e1f886dca4f56eefa31868badd06e9
Marvin Roger <nivramdu94@gmail.com>
2016-11-12 17:20:17 +0100

    :sparkles: Add favicon

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   title: 'Homie Dashboard',
   entry: './app/index.js',
   dist: './dist-app',
   template: './app/index.html',
-  resolve: true
+  resolve: true,
+  mergeConfig: {
+    plugins: [
+      new CopyWebpackPlugin([{ from: 'app/assets/favicon.ico' }])
+    ]
+  }
 }
