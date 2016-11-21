c77bd2fce4593816e29965c8cc164dd2d917718c
Marvin Roger <nivramdu94@gmail.com>
2016-11-21 19:29:44 +0100

    :sparkles: Implement WebSocket messages types

---
+++ common/ws-messages.js
@@ -0,0 +1,43 @@
+import uuid from 'uuid'
+
+export const MESSAGE_TYPES = {
+  EVENT: 0,
+  REQUEST: 1,
+  RESPONSE: 2
+}
+
+export function parseMessage (text) {
+  const parsed = JSON.parse(text)
+  switch (parsed[0]) {
+    case MESSAGE_TYPES.EVENT:
+      return {
+        type: MESSAGE_TYPES.EVENT,
+        event: parsed[1],
+        value: parsed[2]
+      }
+    case MESSAGE_TYPES.REQUEST:
+      return {
+        type: MESSAGE_TYPES.REQUEST,
+        id: parsed[1],
+        method: parsed[2],
+        parameters: parsed[3]
+      }
+    case MESSAGE_TYPES.RESPONSE:
+      return {
+        type: MESSAGE_TYPES.RESPONSE,
+        id: parsed[1],
+        value: parsed[2]
+      }
+  }
+}
+
+export function generateMessage (options) {
+  switch (options.type) {
+    case MESSAGE_TYPES.EVENT:
+      return JSON.stringify([MESSAGE_TYPES.EVENT, options.event, options.value])
+    case MESSAGE_TYPES.REQUEST:
+      return JSON.stringify([MESSAGE_TYPES.REQUEST, uuid(), options.method, options.parameters])
+    case MESSAGE_TYPES.RESPONSE:
+      return JSON.stringify([MESSAGE_TYPES.RESPONSE, options.id, options.value])
+  }
+}
---
+++ test/ws-messages.js
@@ -0,0 +1,27 @@
+import test from 'ava'
+
+import {parseMessage, generateMessage, MESSAGE_TYPES} from '../common/ws-messages'
+
+test('generate and parse an event', t => {
+  const options = { type: MESSAGE_TYPES.EVENT, event: 'super_event', value: 'Wooh' }
+  const message = generateMessage(options)
+  const parsed = parseMessage(message)
+  t.deepEqual(parsed, options)
+})
+
+test('generate and parse a request', t => {
+  const options = { type: MESSAGE_TYPES.REQUEST, method: 'getStats', parameters: { timespan: 'all' } }
+  const message = generateMessage(options)
+  const parsed = parseMessage(message)
+  t.is(parsed.type, options.type)
+  t.is(parsed.method, options.method)
+  t.deepEqual(parsed.parameters, options.parameters)
+  t.regex(parsed.id, /^[a-z0-9]{8}-([a-z0-9]{4}-){3}[a-z0-9]{12}$/)
+})
+
+test('generate and parse a response', t => {
+  const options = { type: MESSAGE_TYPES.RESPONSE, id: '110ec58a-a0f2-4ac4-8393-c866d813b8d1', value: 'Wooh' }
+  const message = generateMessage(options)
+  const parsed = parseMessage(message)
+  t.deepEqual(parsed, options)
+})
