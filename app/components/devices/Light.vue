be86e8e01c949c226a7a6819b10195d5fa5fd80e
Marvin Roger <nivramdu94@gmail.com>
2017-01-03 15:05:49 +0100

    :art: Rework light component

--- app/components/devices/Light.vue
+++ app/components/devices/Light.vue
@@ -1,107 +1,85 @@
 <template>
   <node :hasActions="true" :nodeData="nodeData">
-    
     <template slot="img">
       <div class="has-text-centered">
         <i v-if="nodeData.properties.intensity" class="fa fa-lightbulb-o " aria-hidden="true" :style="imgDataStyle"></i>
         <img v-else src="../../assets/images/icons/common/unknown.png" alt="" >
       </div>
     </template>
+
     <template slot="main">
       <div class="has-text-centered">
-      <div id="overlay" style="">
-        <chrome-picker :class="overlay" v-model="colors" @change-color="onChange" style="witdh:60px;height:60px"></chrome-picker>
-      </div>
-
-      <p class="title">
+        <p class="title">
           <template v-if="nodeData.properties.intensity">
-                {{ nodeData.properties.intensity.value }} %
+            {{ nodeData.properties.intensity.value }} %
           </template>
           <template v-else>
-                ?
+            ?
           </template>
         </p>
+
+        <slider-picker v-model="colors" @change-color="onChange"></slider-picker>
+        <input type="range" v-model="intensityInput" min="0" max="100" :data-balloon="`${intensityInput}%`" data-balloon-pos="up" @change="setIntensity()"/>
       </div>
     </template>
+
     <template slot="footer">
-      <a href="" class="card-footer-item" @click.prevent="showOverlay()"> Change Color </a> 
+
     </template>
   </node>
-  
+
 </template>
 
 <script>
-import {Chrome} from 'vue-color'
-import {Component as Node, mixin as nodeMixin} from './Node.js'
 import {mapActions} from 'eva.js'
+import {Slider as SliderPicker} from 'vue-color'
+import {Component as Node, mixin as nodeMixin} from './Node.js'
+import {hslToRgb, rgbToHsl} from '../../helpers/conversions'
 
 export default {
   mixins: [nodeMixin],
   data () {
+    const rgb = this.nodeData.properties.color ? this.nodeData.properties.color.value.split(',').map(str => parseInt(str, 10)) : [0, 255, 255]
+    const hsl = rgbToHsl(rgb[0], rgb[1], rgb[2])
+    hsl[0] *= 360
+
     return {
       colors: {
-        hex: '#194d33',
-        hsl: {
-          h: 150,
-          s: 0.5,
-          l: 0.2,
-          a: 1
-        },
-        hsv: {
-          h: 150,
-          s: 0.66,
-          v: 0.30,
-          a: 1
-        },
-        rgba: {
-          r: 25,
-          g: 77,
-          b: 51,
-          a: 1
-        },
+        h: hsl[0],
+        s: 1,
+        l: 0.5,
         a: 1
       },
-      overlay: "is-hidden"
-    } 
+      intensityInput: this.nodeData.properties.intensity ? this.nodeData.properties.intensity.value : 0
+    }
   },
   computed: {
-    
-    imgDataStyle(){
-      if(this.nodeData.properties.intensity && parseInt(this.nodeData.properties.intensity.value) <= 1)
-      {
+    imgDataStyle () {
+      if (this.nodeData.properties.intensity && parseInt(this.nodeData.properties.intensity.value) <= 1) {
         return {
-                fontSize: '1725%',
-                opacity: 10 / 100,
-                color: 'rgb(0,0,0)'
+          fontSize: '1725%',
+          opacity: 10 / 100,
+          color: 'rgb(0,0,0)'
         }
-      }
-      else
-      {
+      } else {
         return {
-                fontSize: '1725%',
-                opacity: this.nodeData.properties.intensity.value / 100,
-                color: this.nodeData.properties.color ? `rgb(${parseInt(this.nodeData.properties.color.value.split(',')[0])},${parseInt(this.nodeData.properties.color.value.split(',')[1])},${parseInt(this.nodeData.properties.color.value.split(',')[2])}` : 'rgb(0,0,0)'
-         }
-      }
-    },
-    styleRedBackground () {
-      return {
-        backgroundColor: '#e74c3c',
-        color: 'black'
+          fontSize: '1725%',
+          opacity: this.nodeData.properties.intensity.value / 100,
+          color: this.nodeData.properties.color ? `rgb(${parseInt(this.nodeData.properties.color.value.split(',')[0])},${parseInt(this.nodeData.properties.color.value.split(',')[1])},${parseInt(this.nodeData.properties.color.value.split(',')[2])}` : 'rgb(0,0,0)'
+        }
       }
     }
   },
-  components: {Node, 'chrome-picker': Chrome},
+  components: {Node, SliderPicker},
   methods: {
-    setIntensity (rangeValue) {
+    setIntensity () {
       this.setState({
         deviceId: this.nodeData.device.id,
         nodeId: this.nodeData.id,
         property: 'intensity',
-        value: rangeValue.toString()
+        value: this.intensityInput.toString()
       })
     },
-    ...mapActions(['setState']),
     changeColor (rgb) {
       this.setState({
         deviceId: this.nodeData.device.id,
@@ -110,23 +88,24 @@ export default {
         value: rgb
       })
     },
-    ...mapActions(['setState']),
-    onChange(rgb)
-    {
-      this.changeColor(rgb.rgba.r+','+rgb.rgba.g+','+rgb.rgba.b)
-      this.setIntensity(Math.round(rgb.rgba.a * 100))
+    onChange (color) {
+      const h = parseInt(color.hsl.h, 10) / 360
+      const s = 1
+      const l = 0.5
+      const rgb = hslToRgb(h, s, l)
+      console.log(rgb)
+      this.changeColor(rgb[0] + ',' + rgb[1] + ',' + rgb[2])
     },
-    showOverlay()
-    {
-      if(this.overlay === "is-hidden") this.overlay = "is-overlay"
-      else this.overlay = "is-hidden"
-    }
-
+    ...mapActions(['setState'])
   }
 }
 
 </script>
 
 <style lang="sass">
+  .vue-color__slider
+    width: 100% !important
 
+  .vue-color__slider__swatches
+    display: none !important
 </style>
