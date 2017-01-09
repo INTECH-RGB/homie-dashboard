560451d652e78a6c619df6d4e708e9e485767b28
Marvin Roger <nivramdu94@gmail.com>
2017-01-09 15:20:24 +0100

    :racehorse: Optimize dependencies

--- vue.config.js
+++ vue.config.js
@@ -8,14 +8,13 @@ export default {
   template: './app/index.html',
   notify: true,
   resolve: true,
-  vendor: ['axios', 'eva.js', 'eventemitter3', 'fast-json-patch', 'uuid', 'vue', 'vue-color', 'vue-grid-layout/dist/vue-grid-layout.min.js'],
+  vendor: ['axios', 'chart.js', 'eva.js', 'eventemitter3', 'fast-json-patch', 'uuid', 'vue', 'vue-async-computed', 'vue-color', 'vue-grid-layout/dist/vue-grid-layout.min.js'],
   mergeConfig: {
     performance: {
+      hints: false,
       assetFilter: function (assetFilename) {
         return assetFilename.endsWith('.js')
-      },
-      maxEntrypointSize: 500000,
-      maxAssetSize: 400000
+      }
     }
   }
 }
