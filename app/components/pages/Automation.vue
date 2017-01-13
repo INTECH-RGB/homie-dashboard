997fa4681590b8aec72266acda3e1495a69a92e7
Marvin Roger <nivramdu94@gmail.com>
2017-01-13 10:26:34 +0100

    :sparkles: Add an automation page

---
+++ app/components/pages/Automation.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <help :visible="help" @close="help = false"></help>
+
+    <h1 class="title">
+      Automatisation
+      <a href="#" @click.prevent="help = true" data-balloon="Aide" data-balloon-pos="up">
+        <span class="icon is-medium">
+          <i class="fa fa-question-circle-o"></i>
+        </span>
+      </a>
+    </h1>
+    <h2 class="subtitle">
+      Ici, vous pouvez automatiser les comportements de vos périphériques.
+    </h2>
+  </div>
+</template>
+
+<script>
+import Help from '../help/Automation'
+
+export default {
+  data () {
+    return {
+      help: false
+    }
+  },
+  components: {
+    Help
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
