1ee581595713281b3503e8ce07ebd997bbf5c6f9
Marvin Roger <nivramdu94@gmail.com>
2017-01-17 10:19:44 +0100

    :fire: Replace argon by pure bcryptjs

--- server/lib/hash.js
+++ server/lib/hash.js
@@ -1,12 +1,22 @@
-import argon2 from 'argon2'
+import bcrypt from 'bcryptjs'
 
-export async function hash (password) {
-  const salt = await argon2.generateSalt()
-  const hash = await argon2.hash(password, salt)
-
-  return hash
+export function hash (password) {
+  return new Promise((resolve, reject) => {
+    bcrypt.genSalt(function (err, salt) {
+      if (err) return reject(err)
+      bcrypt.hash(password, salt, function (err, hash) {
+        if (err) return reject(err)
+        resolve(hash)
+      })
+    })
+  })
 }
 
 export async function verify (hash, password) {
-  return await argon2.verify(hash, password)
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, hash, function (err, res) {
+      if (err) return reject(err)
+      resolve(res)
+    })
+  })
 }
