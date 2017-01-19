1370793d2587c091150016e929f65f44d30163b2
Marvin Roger <nivramdu94@gmail.com>
2017-01-19 15:48:30 +0100

    :bug: Convert schema from json to js so that it's built into dist folder

---
+++ server/lib/validators/schemas/settings.js
@@ -0,0 +1,84 @@
+export default {
+  'title': 'Settings',
+  'type': 'object',
+  'properties': {
+    'bindings': {
+      'type': 'object',
+      'properties': {
+        'aqara': {
+          'type': 'object',
+          'properties': {
+            'enabled': {
+              'type': 'boolean'
+            },
+            'gateway': {
+              'type': 'object',
+              'properties': {
+                'ip': {
+                  'type': 'string'
+                },
+                'password': {
+                  'type': 'string'
+                }
+              }
+            }
+          },
+          'required': ['enabled', 'gateway']
+        },
+        'yeelight': {
+          'type': 'object',
+          'properties': {
+            'enabled': {
+              'type': 'boolean'
+            }
+          },
+          'required': ['enabled']
+        }
+      },
+      'required': ['aqara', 'yeelight']
+    },
+    'mqtt': {
+      'type': 'object',
+      'properties': {
+        'host': {
+          'type': 'string'
+        },
+        'port': {
+          'type': 'integer'
+        }
+      },
+      'required': ['host', 'port']
+    },
+    'homie-esp8266': {
+      'type': 'object',
+      'properties': {
+        'wifi': {
+          'type': 'object',
+          'properties': {
+            'ssid': {
+              'type': 'string'
+            },
+            'password': {
+              'type': 'string'
+            }
+          },
+          'required': ['ssid', 'password']
+        },
+        'mqtt': {
+          'type': 'object',
+          'properties': {
+            'host': {
+              'type': 'string'
+            },
+            'port': {
+              'type': 'integer'
+            }
+          },
+          'required': ['host', 'port']
+        }
+      },
+      'required': ['wifi', 'mqtt']
+    }
+  },
+  'required': ['bindings', 'mqtt', 'homie-esp8266']
+}
